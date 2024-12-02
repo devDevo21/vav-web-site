0d5b55e5b32e78a22550ec0698e55ee32424ee4f
devDevo21 <loiclekane.pro@gmail.com>
2024-12-02 11:15:36 +0100

    ajout de lexportation static

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const SITE = require('./src/config.js').SITE;
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
 
   trailingSlash: SITE.trailingSlash,
   basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
