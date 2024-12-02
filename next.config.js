ee33cde3c0f96985f255544e244d36ac4df2e8ed
devDevo21 <loiclekane.pro@gmail.com>
2024-12-02 12:30:04 +0100

    activation du output : export

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const SITE = require('./src/config.js').SITE;
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // output: 'export',
+  output: 'export',
 
   trailingSlash: SITE.trailingSlash,
   basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
